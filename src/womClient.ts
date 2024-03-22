2b30cdee9ee4b8fcf95012c1a5326dedc8bafdb9
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2024-03-22 13:14:10 -0500

    api key

--- src/womClient.ts
+++ src/womClient.ts
@@ -1,3 +1,6 @@
 import { WOMClient } from "@wise-old-man/utils";
 
-export const client = new WOMClient();
+export const client = new WOMClient({
+  apiKey: import.meta.env.VITE_WOM_API_KEY,
+  userAgent: import.meta.env.VITE_DISCORD_USERNAME,
+});
