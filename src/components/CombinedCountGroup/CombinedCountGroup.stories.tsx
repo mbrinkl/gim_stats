4d526cc95e38bbacf0bd44b2d0a1cd35efafbc0f
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2024-12-12 23:34:04 -0600

    formatting

--- src/components/CombinedCountGroup/CombinedCountGroup.stories.tsx
+++ src/components/CombinedCountGroup/CombinedCountGroup.stories.tsx
@@ -4,19 +4,54 @@ import { Combined } from "../../types";
 export const CombinedCountGroupStory = () => {
   const combinedCounts: Combined[] = [
     {
-      metric: { name: "Chambers of Xeric: Challenge Mode", aliases: [] },
+      metric: { name: "Chambers of Xeric", aliases: [] },
       playerData: [
         {
           username: "WWWWWWWWWWWW",
           count: 99999,
         },
         {
-          username: "asdf-asdfas",
-          count: 123,
+          username: "WWWWWWWWWWWW",
+          count: 4_600_000_000,
+        },
+        {
+          username: "asdfas-asdf",
+          count: 345,
+        },
+      ],
+      total: 1000,
+    },
+    {
+      metric: { name: "Clue Scrolls: Easy", aliases: [] },
+      playerData: [
+        {
+          username: "a",
+          count: 1,
+        },
+        {
+          username: "b",
+          count: 2,
+        },
+      ],
+      total: 1000,
+    },
+    {
+      metric: { name: "Strength", aliases: [] },
+      playerData: [
+        {
+          username: "WWWWWWWWWWWW",
+          count: 99999,
+          level: 99,
+        },
+        {
+          username: "WWWWWWWWWWWW",
+          count: 4_600_000_000,
+          level: 10,
         },
         {
           username: "asdfas-asdf",
           count: 123,
+          level: 1,
         },
       ],
       total: 1000,
--- src/components/IndividualMetricComparison.tsx
+++ src/components/IndividualMetricComparison.tsx
@@ -8,16 +8,27 @@ interface IndividualMetricComparisonProps {
 
 export const IndividualMetricComparison = (props: IndividualMetricComparisonProps) => {
   const sorted = props.combined.playerData.sort((a, b) => (b.level || 0) - (a.level || 0) || b.count - a.count);
+  const widths = props.combined.playerData.some((data) => data.level != null)
+    ? ["40%", "35%", "25%"]
+    : ["65%", "35%", "0"];
 
   return (
     <TableContainer w="100%">
-      <Table variant="striped" size="sm">
+      <Table variant="striped" size="sm" __css={{ width: "full", tableLayout: "fixed" }}>
         <Tbody>
           {sorted.map((data) => (
             <Tr key={data.username}>
-              <Td>{data.username}</Td>
-              <Td isNumeric>{formatCount(data.count)}</Td>
-              {data.level != null && <Td>Lv{data.level}</Td>}
+              <Td wordBreak="break-all" whiteSpace="normal" w={widths[0]}>
+                {data.username}
+              </Td>
+              <Td isNumeric w={widths[1]}>
+                {formatCount(data.count)}
+              </Td>
+              {data.level != null && (
+                <Td isNumeric w={widths[2]}>
+                  Lv{data.level}
+                </Td>
+              )}
             </Tr>
           ))}
         </Tbody>
