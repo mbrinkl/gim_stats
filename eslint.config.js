e6281ca29a720119efdcb30f257c54e02a46012b
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2025-06-19 01:10:47 -0500

    resolver and eslint imports

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,17 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import storybook from "eslint-plugin-storybook";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      ...tseslint.configs.recommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -21,6 +27,15 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "import/order": [
+        "error",
+        {
+          named: true,
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
     },
   },
   storybook.configs["flat/recommended"],
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
       verboseFileRoutes: false,
     }),
     react(),
+    tsconfigPaths(),
   ],
   build: {
     sourcemap: true,
